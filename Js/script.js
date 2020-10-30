c6eba111d0039e03849f7374f23781022267dc2c
anonym <art.graphik44@gmail.com>
2020-10-30 11:19:15 +0100

    Copy new file

---
+++ Js/script.js
@@ -0,0 +1,24 @@
+console.log("Hello word");
+// function value (valeur)
+// {
+// //     console.log(valeur);
+// //     var button = document.querySelector(".button");
+// // button.addEventListener("click", ()=>{
+// //     console.log(this)
+// // });
+// }
+
+// var array=[1,2,3,4];
+
+// array.forEach( element => {
+//     console.log(element);
+// });
+
+let episode = {
+    title:"Prison Break",
+    duration: 30,
+    hasBeenWatched: true
+}
+
+
+    
